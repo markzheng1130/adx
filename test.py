ec4376d65ef489e1524bde7229b9d5eef4892a59
Mark Cheng (RD-TW) <mark_cheng@trendmicro.com>
2021-03-19 15:30:27 +0800

    add

---
+++ test.py
@@ -0,0 +1,13 @@
+from azure.kusto.data import KustoClient, KustoConnectionStringBuilder
+
+cluster = "https://mkadxcluster.eastus.kusto.windows.net"
+kcsb = KustoConnectionStringBuilder.with_az_cli_authentication(cluster)
+client = KustoClient(kcsb)
+
+db = "TestDatabase"
+query = "StormEvents | take 3"
+
+response = client.execute(db, query)
+
+for row in response.primary_results[0]:
+    print(f'{row}')
